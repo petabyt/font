7a1b590c20757cd2fce8f49f7c5e4f7f269b9c2a
Daniel C <brikbusters@gmail.com>
2022-07-30 15:36:23 -0400

    add lib.c

---
+++ font.c
@@ -0,0 +1,2 @@
+/* If a *.c file is needed */
+#include "font.h"
---
+++ lib.c
@@ -0,0 +1,56 @@
+void font_draw_pixel(int x, int y);
+
+int font_print_char(int x, int y, char c) {
+	// Loop to "null terminator character"
+	int match = 0;
+	for (int l = 0; font[l].letter != 0; l++) {
+		if (font[l].letter == c) {
+			match = l;
+			break;
+		}
+	}
+
+	// Loop through 7 high 5 wide monochrome font
+	int maxLength = 0;
+	for (int py = 0; py < 7; py++) {
+		for (int px = 0; px < 5; px++) {
+			if (font[match].code[py][px] == '#') {
+				font_draw_pixel(x + px, y + py);
+
+				// Dynamix width character spacing
+				if (px > maxLength) {
+					maxLength = px;
+				}
+			}
+		}
+	}
+
+	return maxLength;
+}
+
+int font_print_string(int x, int y, char *string) {
+	int cx = x;
+	int cy = y;
+
+	for (int c = 0; string[c] != '\0'; c++) {
+		int length;
+		if (string[c] == ' ') {
+			length = 5;
+		} else {
+			length = font_print_char(cx, cy, string[c]) + 3;
+		}
+
+		cx += length;
+
+		#ifdef SCREEN_LENGTH
+		// Text wrap after space
+		// Fine tune this to your liking.
+		if (cx > SCREEN_WIDTH - 20 && string[c] == ' ') {
+			cx = x;
+			cy += 8;
+		}
+		#endif
+	}
+
+	return cy;
+}
